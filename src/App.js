12ddd0b9f5bd4b4720b78ddd77e99858fc0798f7
Steve Jobs <ccs14.school@gmail.com>
2019-09-21 14:32:34 -0600

    add initial work for project

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+import socketIOClient from "socket.io-client";
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      response: false,
+      endpoint: "http://127.0.0.1:3001/"
+    };
+  }
+
+  componentDidMount() {
+    const { endpoint } = this.state;
+    const socket = socketIOClient(endpoint);
+    for (let i = 0; i < 10; i++) {
+      socket.emit("message", `this is message ${i}, from the client`);
+    }
+    socket.on("group-message", data => this.setState({ response: data }, () => {
+      console.log(`this is message '${data}' from the server`)
+    }));
+  }
+
+  render() {
+    const { response } = this.state;
+    return (
+      <div style={{ textAlign: "center" }}>
+        {response
+          ? <p>{response}</p>
+          : <p>Loading...</p>}
+      </div>
+    );
+  }
+}
+
+export default App;
