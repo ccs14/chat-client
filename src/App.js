32981af72f13bb02eb904dd0db5cc65d9889253c
Steve Jobs <ccs14.school@gmail.com>
2020-06-17 23:31:43 -0600

    update react core package

--- src/App.js
+++ src/App.js
@@ -13,15 +13,23 @@ class App extends Component {
   componentDidMount() {
     const { endpoint } = this.state;
     const socket = socketIOClient(endpoint);
+
     for (let i = 0; i < 10; i++) {
       socket.emit("message", `this is message ${i}, from the client`);
     }
+
     socket.on("group-message", data => this.setState({ response: data }, () => {
       console.log(`this is message '${data}' from the server`)
     }));
   }
 
   render() {
+    /*
+    if (this.state.response) {
+      console.log(`this message came from the sever: ${this.state.response.data}`);
+    }
+    */
+
     const { response } = this.state;
     return (
       <div style={{ textAlign: "center" }}>
