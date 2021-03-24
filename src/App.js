4cb2e200f06df8573c36ac3db8673c9fe4108684
Steve Jobs <ccs14.school@gmail.com>
2021-03-23 21:52:38 -0600

    initial check in

--- src/App.js
+++ src/App.js
@@ -4,40 +4,55 @@ import Chat from "./Components/Chat";
 import makeText from "./Utils/Utils";
 import 'dotenv/config';
 
+// socket object for client
+// const socket = socketIOClient(process.env.SERVER_URL);
+// create user id for each client
+const userId = Math.floor(Math.random() * 1000).toString();
+
 class App extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
       response: false,
+      text: {},
+      event: {},
       messages: [],
-      endpoint: "http://127.0.0.1:3001/"
+      endpoint: "http://127.0.0.1:3001",
     };
   }
 
-  endpoint = "http://127.0.0.1:3001/";
-  socket = socketIOClient(this.endpoint);
-  // create user id for each client
-  userId = Math.floor(Math.random() * 1000).toString();
+  // capture textbox input
+  captureText = (event) => {
+    // capture from textbox and update state var
+    console.log("new text: ", event.target.value);
+    this.setState({ text: event.target.value });
+  };
 
   // make a new message from random text, and send to the server
   createMessage = () => {
+    const socket = socketIOClient(process.env.SERVER_URL);
+    console.log("in createMessage()");
+    console.log("state: ", this.state);
     // get random text
-    const messageText = makeText(15);
+    // const messageText = makeText(15);
     // create standard message object
     const message = {
-      "user": this.userId,
-      "message": messageText
+      "user": userId,
+      "message": this.state.text
     };
-
-    // send a message event to the server, pass username and message
-    this.socket.emit("message", message);
+    // log and send new message
+    console.log("new client message: ", message);
+    debugger;
+    socket.emit("message", [message]);
   };
 
-  componentDidMount() {
+  componentDidMount = () => {
+    const socket = socketIOClient(process.env.SERVER_URL);
     // receive a group-message event, store data
-    this.socket.on("group-message", data => this.setState({ response: data }, () => {
+    socket.on("group-message", data => this.setState({ response: data }, () => {
       let newMessage = `user${data.user} - ${data.message}`;
-      console.log(`${newMessage} (from server)`);
+      console.log("from server: ", newMessage);
+      console.log("from server: ", data);
       this.state.messages.push(data);
     }));
 
@@ -45,21 +60,17 @@ class App extends Component {
   }
 
   render() {
-    console.log(`this.state: ${this.state}`);
+    console.log("this.state: ", this.state);
 
-    if (this.state.response) {
-      console.log(`this message came from the sever: ${this.state.response.data}`);
-    }
-
-    if (this.state.messages)
-    {
-      return (
-        <div>
-          <Chat messages={this.state.messages}/>
-          <input type="submit" value="Create Message" onClick={() => this.createMessage()} />
-        </div>
-      );
-    }
+    return (
+      <div>
+        <Chat messages={this.state.messages}/>
+        <form>
+          <input type='text' onChange={ this.captureText} />
+          <input type="submit" value="Create Message" onClick={this.createMessage} />
+        </form>
+      </div>
+    );
   }
 }
 
